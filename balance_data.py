2b90faa6c28f0a8dea36bc08dd6089c7e7be8aa1
Parth Pahwa <parth.pahwa95@gmail.com>
2017-11-08 14:22:00 +0530

    Update balance_data.py

--- balance_data.py
+++ balance_data.py
@@ -8,13 +8,14 @@ j = 0
 negArray = []
 
 # Under sample training data as majority of input belong to NOOP Class
-while j < int(1.3*len(crucial_data)) and i < len(train_data):
+while j < int(1.6*len(crucial_data)) and i < len(train_data):
     temp = train_data[i]
     if temp[1] == [0]:
         negArray.append(temp)
         j += 1
-    i += 1
-
+        i += 1
+    else:
+        i += 15
 
 train_data = np.concatenate((crucial_data, negArray), axis=0)
 np.random.shuffle(train_data)
